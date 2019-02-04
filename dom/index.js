3d7293ee5639cf7a90030fbf1c0a3d2201059721
Nhan <nhan@codistry.io>
2019-02-03 16:15:43 -0800

    added dom lesson

---
+++ dom/index.js
@@ -0,0 +1,9 @@
+(function(window) {
+  //Create div main and appending it to body
+  let main = document.createElement('div');
+  main.id = 'main';
+  main.textContent = "This is main";
+  document.body.appendChild(main);
+
+  
+})(window);
